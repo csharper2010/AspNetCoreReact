21d0ff2f5b25e329bd5e89203e64f0c8be267fb8
CSharper2010 <csharper2010@googlemail.com>
2018-01-20 07:44:42 +0100

    Hello World Web Projekt

---
+++ src/Getränkehandel.Web/Startup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Getränkehandel.Infrastructure.Data;
+using Getränkehandel.Infrastructure.Repository;
+using Getränkehandel.Repository;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Getränkehandel.Web
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<GetränkehandelContext>();
+
+            services.AddScoped(typeof(IRepository<,>), typeof(BaseRepository<,>));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
